f88fcc85b93c99c10712af2b63720005042d2441
Ne-GH <printfne@gmail.com>
2024-01-05 00:31:39 +0800

    cmkae同时生成server和client，暂时将控制端和被控制端作为c/s，功能基本完成后再修改为p2p模式

--- control_main.cpp
+++ control_main.cpp
@@ -10,6 +10,7 @@
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowTitle("控制端");
     w.show();
     // 开线程，监听key
     // 并做其他处理
---
+++ controlled_main.cpp
@@ -0,0 +1,30 @@
+#include "MainWindow.h"
+
+#include <QThread>
+#include <QApplication>
+#include <QLabel>
+#include <QStatusBar>
+#include <QTimer>
+#include "Event.h"
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.setWindowTitle("被控制端");
+    w.show();
+    // 开线程，监听key
+    // 并做其他处理
+
+    QTimer timer;
+    QObject::connect(&timer,&QTimer::timeout,[&]{
+        CursorState cursor_state;
+        w.statusBar()->showMessage((std::to_string(cursor_state.x) + " , " + std::to_string(cursor_state.y)).c_str());
+    });
+    timer.start(100);
+
+    ListenEvent le;
+    le.start();
+
+
+    return QApplication::exec();
+}
