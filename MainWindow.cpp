e751c4f80d0be037e826a58e3407f2e83938c470
Ne-GH <printfne@gmail.com>
2024-02-04 03:17:00 +0800

    不再依赖class Control 和 class Controlled，但是这两个类并没有从项目中删除

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -21,7 +21,8 @@ MainWindow::MainWindow(QWidget *parent)
 		auto show_lab = new QLabel(this);
 		setCentralWidget(show_lab);
 		show_lab->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
-        auto control = new Control(show_lab,port);
+        // auto control = new Control(show_lab,port);
+        auto display = new Display(show_lab, port);
 	});
 
     QObject::connect(ui->connect, &QPushButton::clicked, [this] {   
@@ -30,7 +31,14 @@ MainWindow::MainWindow(QWidget *parent)
         auto addr = list[0];
         auto port = list[1];
         qDebug() << addr << " " << port;
-        auto p = new Controlled(addr,port);
+    //     auto p = new Controlled(addr,port);
+		// 开一个线程不断发送屏幕截图
+		auto send_screen_shot = new SendScreenShot(addr,port);
+		auto send_message_thread = new QThread();
+		send_screen_shot->moveToThread(send_message_thread);
+		// std::cout << "init : " << QThread::currentThreadId() << std::endl;
+		QObject::connect(send_screen_shot, &SendScreenShot::SendMessageSig,send_screen_shot,&SendScreenShot::Send);
+		send_message_thread->start();
 	});
 
 }
